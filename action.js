3c11f5210d72407365dfc007d30d6a1e5f55f322
Carlos <charliedn@Gmail.com>
2018-08-17 09:31:56 +0100

    jsjd

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 // Catches the button action from the extension's popup
  document.getElementById('fillForm').addEventListener('click', function(){
   console.log("Click!");
-//   chrome.tabs.executeScript({
+  chrome.tabs.executeScript({
 //     file: "injector.js"
 //   });
 });
